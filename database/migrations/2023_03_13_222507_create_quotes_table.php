8673d895f2c6171cfb448f2facd713428a598d72
Gabriela M. Fernández <galalou2010@gmail.com>
2023-03-14 00:16:40 +0100

    table migrations with relationships

---
+++ database/migrations/2023_03_13_222507_create_quotes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->engine = "InnoDB";
+
+            $table->id();
+            $table->string('text');
+            $table->unsignedBigInteger('author_ID');
+            $table->unsignedBigInteger('book_ID');
+            $table->unsignedBigInteger('genre_ID');
+            $table->timestamps();
+
+            $table->foreign('author_ID')->references('id')->on('authors')->onDelete('cascade');
+            $table->foreign('book_ID')->references('id')->on('books')->onDelete('cascade');
+            $table->foreign('genre_ID')->references('id')->on('genres')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quotes');
+    }
+};
