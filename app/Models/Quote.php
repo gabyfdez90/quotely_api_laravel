245e2b8929ab8a31a04004e90234b37a7a6e086f
Gabriela M. Fernández <galalou2010@gmail.com>
2023-06-05 15:50:27 +0200

    reformatted datetime values for insert and update properties

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Quote extends Model
 {
@@ -24,5 +25,22 @@ class Quote extends Model
     public function genre()
     {
         return $this->belongsTo(Genre::class, 'genre_ID');
+    }
+
+
+    public function toArray()
+    // This function is overwritten to reformat the automatic string value of datetime to another one shorter and more manageable
+    {
+        $attributes = parent::toArray();
+
+        if (isset($attributes['created_at'])) {
+            $attributes['created_at'] = Carbon::parse($attributes['created_at'])->format('Y-m-d');
+        }
+
+        if (isset($attributes['updated_at'])) {
+            $attributes['updated_at'] = Carbon::parse($attributes['updated_at'])->format('Y-m-d');
+        }
+
+        return $attributes;
     }
 }
